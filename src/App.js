2ac4bfbfa1f92d4a57f98efe71ae809e18552a69
pawan187 <pawansuthar187@gmail.com>
2021-07-14 17:34:32 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import "./style.css";
-import Home from '/components/Home.js'
-
+import React from 'react';
+import './style.css';
+import Home from '/components/Home.js';
+import NavBar from './components/navBar';
 export default function App() {
   return (
     <div>
+      <NavBar />
       <h1> it is going to be movie rating app</h1>
     </div>
   );
