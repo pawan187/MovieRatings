3ea61edfaa88c83095909ef9971ee5d216182178
pawan187 <pawansuthar187@gmail.com>
2021-07-14 13:21:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ukdgcb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ukdgcb)
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+import Home from '/components/Home.js'
+
+export default function App() {
+  return (
+    <div>
+      <h1> it is going to be movie rating app</h1>
+      <Home />
+    </div>
+  );
+}
