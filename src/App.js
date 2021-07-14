722985c47267eddcee65d5cc8cd666224cd80938
pawan187 <pawansuthar187@gmail.com>
2021-07-14 17:44:19 +0530

    4 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './style.css';
-import Home from '/components/Home.js';
-import NavBar from './components/navBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Home from './components/Home.js';
 export default function App() {
   return (
     <div>
-      <NavBar />
-      <h1> it is going to be movie rating app</h1>
+      <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,25 @@
 import React from 'react';
 
+import NavBar from './navBar';
+
 export default () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <NavBar />
+      <div class="navbar container-fluid col">
+        <div class="d-flex">
+          <input
+            class="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </div>
+      </div>
+      Homepage
+    </div>
+  );
 };
