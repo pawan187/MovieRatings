d2e1023496a7af62491ba01b538a1bdca337cd3d
pawan187 <pawansuthar187@gmail.com>
2021-07-17 12:37:20 +0530

    1 file modified, 1 added

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -2,31 +2,41 @@ import React, { useState } from 'react';
 
 export default ({ user, MovieId }) => {
   const [SearchText, setSearchText] = useState('');
+  const [Rating, setRating] = useState(0);
   const addComment = () => {
     console.log(user);
     if (!user) {
       alert('please log in', user);
     } else {
-      console.log(SearchText, user._id, MovieId, Rating);
+      fetch('')
+      console.log(SearchText, user.id, MovieId, Rating);
     }
   };
   return (
     <div>
-      <div class="input-group mb-3">
-        <input
-          onChange={e => setSearchText(e.target.value)}
-          type="text"
-          class="form-control"
-          placeholder="enter some comment"
-          aria-describedby="basic-addon2"
-        />
-        <button
-          onClick={() => addComment()}
-          class="input-group-text"
-          id="basic-addon2"
-        >
-          add
-        </button>
+      <div class="list-group-item d-flex justify-content-between align-items-start">
+        <div class="card">
+          <div class="row">
+            <input
+              onChange={e => setSearchText(e.target.value)}
+              type="text"
+              class="form-control col"
+              placeholder="enter some comment"
+              aria-describedby="basic-addon2"
+            />
+          </div>
+          <div class="row">
+            <input
+              type="number"
+              onChange={e => setRating(e.target.value)}
+              class="col from-control"
+              id="basic-addon2"
+            />
+            <button class=" col form-control" onClick={() => addComment()}>
+              add
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
